db360f8db90aa722de21583373f645496d20d585
Zzc0595 <2796442619@qq.com>
2025-09-07 10:42:44 +0800

    Guarantee the m<n.

--- rsa_sender.py
+++ rsa_sender.py
@@ -1,5 +1,7 @@
 n=input("Enter private key n: ")
 e=input("Enter private key e: ")
 m=input("Enter your message: ") #input message
+if int(m) > int(n):
+    print(f"The message can't be bigger than 'n'-{n}.Some of the message would be lost.")
 c = pow(int(m),int(e),int(n)) #generate ciphertext
 print(f"The ciephertext has generated.\nPlease tell the reciever the ciephertext is: {c}")
